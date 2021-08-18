e2ec04e0b69b46fe41952ecc122b8347242a2c59
Carlos Silveira <31132270+profcarlos@users.noreply.github.com>
2021-08-18 08:00:38 -0300

    Create retrofit.ino

---
+++ retrofit.ino
@@ -0,0 +1,7 @@
+/**********
+  IFG Campus Goiânia
+  Autor:    Regivagno e Carlos Silveira
+  Objetivo: 
+  Origem: 
+  URL adicional para Gerenciador de Placas: http://arduino.esp8266.com/stable/package_esp8266com_index.json
+*********/
